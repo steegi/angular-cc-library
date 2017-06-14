58b16832640365b9cdba1ba648417c3704f4f2be
Frank Steegmans <fsteegmans@paystand.com>
2017-06-14 16:00:10 -0700

    Adding dist directory to make package installable directly from github.

---
+++ dist/credit-card.validator.d.ts
@@ -0,0 +1,5 @@
+import { AbstractControl } from '@angular/forms';
+export declare class CreditCardValidator {
+    static validateCCNumber(control: AbstractControl): any;
+    static validateExpDate(control: AbstractControl): any;
+}
---
+++ dist/directives/cvc-format.directive.d.ts
@@ -0,0 +1,12 @@
+import { ElementRef } from '@angular/core';
+export declare class CvcFormatDirective {
+    private el;
+    target: any;
+    constructor(el: ElementRef);
+    ngAfterViewInit(): void;
+    onKeypress(e: any): void;
+    onPaste(e: any): void;
+    onChange(e: any): void;
+    onInput(e: any): void;
+    private reformatCvc(e);
+}
